c2db9ca7057eda4023f17e55553da4c5fc805b8e
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2023-12-17 13:55:07 -0500

    final fixed code from nick

--- MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/HelloApplication.java
+++ MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/HelloApplication.java
@@ -1,74 +1,114 @@
 package com.example.motivationaltasktracker;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+/**
+ * @author Jackson Ding, Nick Mavs, Ben Sheng, Crescent School, ICS4U
+ * Version 1.19 - ALL PROBLEMS FIXED (Hopefully)
+ */
 public class HelloApplication extends Application {
+    //declare the stage and scene that can be accessed by other classes
+    public static Stage primaryStage;
+    public static Scene originalScene;
+    private int taskIDSet = 0;//tempoary variable to set the IDs for the tasks
 
-    private int taskIDSet = 0;
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) throws Exception {
+        //initializes the primary stage and make sure its maximized to the window
+        this.primaryStage = primaryStage;
+        primaryStage.setMaximized(true);
+
 
         // Create a StackPane (a simple layout pane)
         VBox root = new VBox();
 
-        // Create a Scene and set the root node
-        Scene scene = new Scene(root, 300, 200);
+        // Create a Scene and set the root node and initialize for the gloabl variable
+        Scene scene = new Scene(root);
+
+        this.originalScene = scene;
 
         // Set the Scene to the Stage
         primaryStage.setScene(scene);
 
         // Set the title of the Stage
-        primaryStage.setTitle("Simple JavaFX App");
+        primaryStage.setTitle("Motivational Task Tracker");
 
         root.setAlignment(Pos.TOP_CENTER);
 
         // Create a Label for task information
         Label title = new Label("Motivational Task Tracker");
         root.getChildren().add(title);
+        title.setStyle(
+                "-fx-font-size: 40px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        Button createTask = new Button("Create Task");
-
-        // Create a TextField for user input
+        // Create a TextField for the name of the users task
         TextField name = new TextField();
+        name.setAlignment(Pos.CENTER);
         name.setPromptText("Name");
+        name.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        // Create a TextField for user input
+        // Create a TextField for the date a of a user's task
         TextField date = new TextField();
-        date.setPromptText("Date");
+        date.setAlignment(Pos.CENTER);
+        date.setPromptText("Date (Number Only)");
+        date.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        // Create a TextField for user input
+        // Create a TextField for the month of the user's task
         TextField month = new TextField();
-        month.setPromptText("month");
+        month.setAlignment(Pos.CENTER);
+        month.setPromptText("Month (Number Only)");
+        month.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        // Create a TextField for user input
+        // Create a TextField for the difficulty of the users task
         TextField difficulty = new TextField();
-        difficulty.setPromptText("Difficulty");
+        difficulty.setAlignment(Pos.CENTER);
+        difficulty.setPromptText("Difficulty (Scale of 1 to 10)");
+        difficulty.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        root.getChildren().addAll(createTask, name, date, month, difficulty);
+        //create a button which makes a task using all the information entered in the text fields
+        Button createTask = new Button("Create Task");
+        createTask.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Roboto';"  // Change font family
+        );
 
-        Tasks.readTasks(root);
-        //Tasks.updateTaskList();
+        //add the button and text fields to the pane
+        root.getChildren().addAll(name, date, month, difficulty, createTask);
+        //reads the tasks and upadtes the tasks for the JSON file
+         Tasks.readTasks(root);
+         Tasks.displayAllTask(root);
 
+        //when the button to create a task is pressed create a new task and add all of the information entered
         createTask.setOnAction(event -> {
-            Tasks.taskList.add(new Tasks(name.getText(), Integer.parseInt(date.getText()), Integer.parseInt(month.getText()), false, Integer.parseInt(difficulty.getText()), taskIDSet++));
-            Tasks.taskList.get(Tasks.taskList.size()-1).showTask(root);
+            Tasks.taskList.add(new Tasks(name.getText(), Integer.parseInt(date.getText()), Integer.parseInt(month.getText()), false, Integer.parseInt(difficulty.getText()), taskIDSet++, root));
             name.clear();
             date.clear();
             month.clear();
             difficulty.clear();
+            Tasks.displayAllTask(root);
             try {
                 Tasks.updateTaskList();
             } catch (IOException e) {
@@ -76,11 +116,11 @@ public class HelloApplication extends Application {
             }
         });
 
-
-
         // Show the Stage
         primaryStage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
--- MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/Tasks.java
+++ MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/Tasks.java
@@ -1,20 +1,35 @@
+
 package com.example.motivationaltasktracker;
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import java.util.Collections;
 
-import java.io.*;
-import java.util.ArrayList;
-
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.scene.image.ImageView;
+import javafx.geometry.Rectangle2D;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+
+//create a task class with all the parameters that each task has like the difficulty
 public class Tasks {
+    //Properties of a task below
     private String name;
     private long month;
     private long date;
@@ -26,27 +41,72 @@ public class Tasks {
     private CheckBox doneCheckBox;
 
     private Label display;
+    private HBox hbox;
 
-    // Create an HBox to hold the Label and CheckBox
-    HBox hbox;
+    //create new pane for when people complete their tasks, static because it is the same across all objects
+    private static VBox motivatePane = new VBox();
+    private static Scene motivateScene = new Scene(motivatePane);
 
+    //create an arraylist of the tasks static because it applies to the entire class
     public static ArrayList<Tasks> taskList = new ArrayList<Tasks>();
 
-    public Tasks(String name, long date, long month, boolean isDone, long difficulty, long taskID){
+    //create an array list with all possible quotes so that it can randomized for each task
+    public static ArrayList<String> quotes = new ArrayList<String>();{
+        quotes.add("I don't stop when I'm tired, I stop when I'm done.");
+        quotes.add("Everyone fails sometimes and life isn’t supposed to be fair, much less bend to your every whim");
+        quotes.add("Nobody cares what you did yesterday. What have you done today");
+        quotes.add("You gotta start your journey. It may suck, but eventually you will come out the other side on top.");
+        quotes.add("We don’t rise to the level of our expectations, we fall to the level of our training.");
+        quotes.add("No one is going to come help you. No one’s coming to save you.");
+        quotes.add("There is no better way to grow as a person than to do something everyday that you hate.");
+        quotes.add("You can’t use up creativity. The more you use, the more you have.");
+    }
+
+    /**
+     * constructor to initalize all values and properties, including the checkbox, label, and hbox which are consisted of the other properties
+     * the onchecked function is also in the constructor
+     * @param name
+     * @param date
+     * @param month
+     * @param isDone
+     * @param difficulty
+     * @param taskID
+     * @param pane
+     */
+    public Tasks(String name, long date, long month, boolean isDone, long difficulty, long taskID, Pane pane){
         this.date = date;
         this.name = name;
         this.month = month;
         this.isDone = isDone;
         this.difficulty = difficulty;
         this.taskID = taskID;
-        doneCheckBox = new CheckBox();
-        doneCheckBox.setSelected(isDone);
-        display = new Label(this.name + " date: " + this.date + "/" + this.month + " difficulty: " + this.difficulty);
-        hbox = new HBox(10); // 10 is the spacing between elements
-        hbox.setAlignment(Pos.CENTER); // Center the elements horizontally
-        hbox.getChildren().addAll(display, doneCheckBox);
-    }
 
+         doneCheckBox = new CheckBox();
+         doneCheckBox.setSelected(isDone);
+         display = new Label(this.name + " | Date: " + this.date + "/" + this.month + " | Difficulty: " + this.difficulty);
+         display.setStyle(
+         "-fx-font-size: 15px;" + // Change font size
+         "-fx-font-family: 'Roboto';"  // Change font family
+         );
+         hbox = new HBox(10); // 10 is the spacing between elements
+         hbox.setAlignment(Pos.CENTER); // Center the elements horizontally
+         hbox.getChildren().addAll(display, doneCheckBox);
+
+        // Set the action for the checkbox
+        doneCheckBox.setOnAction(event -> {
+            if (doneCheckBox.isSelected()) {
+                taskList.remove(this); // Remove the task from the taskList
+                pane.getChildren().remove(this.getHbox());
+                try {
+                    updateTaskList();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                motivated(pane);
+            }
+        });
+    }
+    //getters and setters
     public long getMonth() {
         return month;
     }
@@ -91,117 +151,266 @@ public class Tasks {
         return taskID;
     }
 
-    public void showTask(Pane pane){
-        pane.getChildren().add(hbox);
+    public HBox getHbox() {
+        return hbox;
+    }
 
-        doneCheckBox.setOnAction(event -> {
-            if (doneCheckBox.isSelected()) {
-                pane.getChildren().remove(hbox);
-                taskList.remove(Tasks.getTask(this.taskID));
-                try {
-                    Tasks.updateTaskList();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            } else {
-                //moticational Quotes
+    public void setHbox(HBox hbox) {
+        this.hbox = hbox;
+    }
+
+    /**
+     * create a method to compare the dates of all the classes so that they can be arranged on when they must be completed
+     */
+    public static void sortTasks(){
+
+        //compare the months first
+
+        for(int i = 0; i < taskList.size()-1; i++)
+        {
+            int min = i;
+
+            for(int j = i+1; j < taskList.size(); j++)
+            {
+                if(taskList.get(j).getMonth() < taskList.get(min).getMonth())
+                    min = j;
             }
+
+            if(min != i)
+            {
+                // Swap
+                Tasks temp = taskList.get(i);
+                taskList.set(i,taskList.get(min));
+                taskList.set(min,temp);
+            }
+        }
+
+        //compare the dates
+        for(int i = 0; i < taskList.size()-1; i++)
+        {
+            int min = i;
+
+            for(int j = i+1; j < taskList.size(); j++)
+            {
+                if(taskList.get(j).getDate() < taskList.get(min).getDate()&& taskList.get(j).getMonth() == taskList.get(min).getMonth()){
+                    min = j;
+                }
+
+            }
+
+            if(min != i)
+            {
+                // Swap
+                Tasks temp = taskList.get(i);
+                taskList.set(i,taskList.get(min));
+                taskList.set(min,temp);
+            }
+        }
+    }
+
+    /**
+     * Display all of the tasks in the task list
+     * the sortTask function also included to reduce redundancy
+     */
+    public static void displayAllTask(Pane pane) {
+
+        pane.getChildren().removeIf(node -> node instanceof HBox); //remove all hbox before hand
+
+        sortTasks(); // Sort the tasks based on due dates
+
+        //display the hboxes
+        for (int i = 0; i<taskList.size(); i++) {
+            pane.getChildren().add(taskList.get(i).getHbox());
+        }
+    }
+
+    /**
+     * method for opening images and returning images based in image URL
+     * @param imageUrl
+     * @return
+     */
+    private Image loadImageFromUrl(String imageUrl) {
+        try {
+            // Open a stream from the URL
+            InputStream inputStream = new URL(imageUrl).openStream();
+
+            // Load the image from the stream
+            return new Image(inputStream);
+        } catch (IOException e) {
+            // Handle exceptions, e.g., if the URL is invalid
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * method for once a task is done
+     * @param pane
+     */
+    public void motivated(Pane pane){
+
+        //create a button to used by the user to go back to the appliction
+        Button motivated = new Button();
+        motivated.setMinHeight(25);
+        motivated.setMinWidth(100);
+        motivated.setText("Motivated");
+        motivated.setStyle("-fx-content-display: center;");
+        motivated.setStyle(
+                "-fx-border-color: black;" + // Border color
+                        "-fx-border-width: 2px;" + // Border width
+                        "-fx-border-radius: 5px;" // Border radius (rounded corners)
+        );
+
+        //create text to hold the quote so it can be displayed
+        Text displayQuotes = new Text();
+        displayQuotes.setStyle(
+                "-fx-font-size: 20px;" + // Change font size
+                        "-fx-font-family: 'Impact';"  // Change font family
+        );
+        //use mosue position to change the colour of the button based on if the mouse is hovering over it
+        //when the mouse is in the button make the button black and the text white
+        motivated.setOnMouseEntered((MouseEvent event) -> {
+            motivated.setTextFill(Color.WHITE);
+            motivated.setStyle("-fx-background-color: #000000; ");
+        });
+        //when the mouse leaves the button return it back to its normal properties
+        motivated.setOnMouseExited((MouseEvent event) -> {
+            motivated.setTextFill(Color.BLACK);
+            motivated.setStyle("-fx-background-color: #FFFFFF; "+"-fx-border-color: black;"
+                    +"-fx-border-width: 2px;"+"-fx-border-radius: 5px;");
+        });
+        //get a random number betweeen 0-6 to represent the array list of quotes
+        int max = 7;
+        int min = 0;
+        int range = max - min + 1;
+        //use math class to randomly pick a number to represent a quote
+        int random = (int) (Math.random()*range) + min;
+        //update the text of displayQuotes to have the random quote so that it can be displayed
+        displayQuotes.setText(quotes.get(random));
+        //add both the quote and the motivated button to the motviated pane for when they complete a task
+        motivatePane.setAlignment(Pos.CENTER);
+        motivatePane.getChildren().addAll(displayQuotes, motivated);
+
+        //create a string to the redirect of the image we want
+        String imageURL = "https://www.dmarge.com/wp-content/uploads/2021/08/goggins2-1.jpg";
+        //load the image using a image view and set it on the screen
+        Image backgroundImage = loadImageFromUrl(imageURL);
+        ImageView imageView = new ImageView(backgroundImage);
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        //use primaryScreenBounds to make sure that the image fills any sized window
+        imageView.setFitWidth(primaryScreenBounds.getWidth()); // Set your preferred width
+        imageView.setFitHeight(primaryScreenBounds.getHeight()); // Set your preferred height
+        motivatePane.getChildren().add(imageView);
+        //switch the the movtated scene with the image and randomized quote at the top
+        HelloApplication.primaryStage.setScene(motivateScene);
+
+        //change scenes back to the creation and main scene after the user has been motvated
+        motivated.setOnAction(event -> {
+            HelloApplication.primaryStage.setScene(HelloApplication.originalScene);
+            HelloApplication.primaryStage.setMaximized(true);
+            //displayAllTask(pane);
+            motivatePane.getChildren().clear();
         });
     }
 
-    public static Tasks getTask(long taskID){
-        for(int i = 0; i<taskList.size(); i++){
-            if(taskList.get(i).getTaskID()==taskID){
-                return taskList.get(i);
-            }
-        }
-        return null;
-    }
+    /**
+     * updates the task list and writes all of the information into the info.json file
+     * @throws IOException
+     */
+     public static void updateTaskList() throws IOException{
 
-    public static void updateTaskList() throws IOException{
+         //get the file based on local path and create the required variables for writing into json
+         File info = new File("/Users/jacksonding/Documents/AP CompSCi/MotivationalTaskTracker copy/MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/info.json");
 
-        File info = new File("/Users/jacksonding/Documents/AP CompSCi/MotivationalTaskTracker/MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/info.json");
+         FileWriter file = new FileWriter(info);
 
-        //JSONArray taskListJson = new JSONArray();
-        FileWriter file = new FileWriter(info);
+         JSONArray taskListJson = new JSONArray();
 
-        JSONArray taskListJson = new JSONArray();
+         //loops through all tasks and stores their key info like name, date, and difficulty
+         for(int i = 0; i< taskList.size(); i++){
 
-        for(int i = 0; i< taskList.size(); i++){
+             JSONObject tempInfo = new JSONObject();
+             tempInfo.put("Name", taskList.get(i).getName());
+             tempInfo.put("Month", taskList.get(i).getMonth());
+             tempInfo.put("Date", taskList.get(i).getDate());
+             tempInfo.put("IsDone", taskList.get(i).isDone());
+             tempInfo.put("Difficulty", taskList.get(i).getDifficulty());
+             tempInfo.put("taskID", taskList.get(i).getTaskID());
 
-            JSONObject tempInfo = new JSONObject();
-            tempInfo.put("Name", taskList.get(i).getName());
-            tempInfo.put("Month", taskList.get(i).getMonth());
-            tempInfo.put("Date", taskList.get(i).getDate());
-            tempInfo.put("IsDone", taskList.get(i).isDone());
-            tempInfo.put("Difficulty", taskList.get(i).getDifficulty());
-            tempInfo.put("taskID", taskList.get(i).getTaskID());
+             taskListJson.add(tempInfo);
+         }
 
-            //JSONObject tempTask = new JSONObject();
-            //tempTask.put("Task", tempInfo);
+         try{
+             //We can write any JSONArray or JSONObject instance to the file
+             file.write(taskListJson.toJSONString());
+             file.flush();
 
-            taskListJson.add(tempInfo);
-        }
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
 
-        try{
-            //We can write any JSONArray or JSONObject instance to the file
-            file.write(taskListJson.toJSONString());
-            file.flush();
+     }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * method used to read the information from the json file, used in the very start of code
+     * @param pane
+     */
+     public static void readTasks(Pane pane){
+         //JSON parser object to parse read file
+         JSONParser jsonParser = new JSONParser();
 
-    }
+         File info = new File("/Users/jacksonding/Documents/AP CompSCi/MotivationalTaskTracker copy/MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/info.json");
 
-    public static void readTasks(Pane pane){
-        //JSON parser object to parse read file
-        JSONParser jsonParser = new JSONParser();
+         try (FileReader reader = new FileReader(info))
+         {
 
-        File info = new File("/Users/jacksonding/Documents/AP CompSCi/MotivationalTaskTracker/MotivationalTaskTracker/src/main/java/com/example/motivationaltasktracker/info.json");
+             //Read JSON file
+             Object obj = jsonParser.parse(reader);
 
-        try (FileReader reader = new FileReader(info))
-        {
+             JSONArray taskList = (JSONArray) obj;
 
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
+             if (taskList instanceof JSONArray) {
+                 //loops through the json file to get all of the jsonObjects and uses the lamda expression to call on the parseTaskObject method which will add the tasks into the actual task List
+                 taskList.forEach( emp -> parseTaskObject( (JSONObject) emp, pane ) );
+             } else {
+                 System.out.println("The file does not contain a JSON object.");
+             }
 
-            JSONArray taskList = (JSONArray) obj;
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         } catch (IOException | ParseException e) {
+             e.printStackTrace();
+         }
+     }
 
-            if (taskList instanceof JSONArray) {
-//                JSONObject jsonObject = (JSONObject) obj;
-                taskList.forEach( emp -> parseTaskObject( (JSONObject) emp, pane ) );//emp is a array
-            } else {
-                System.out.println("The file does not contain a JSON object.");
-            }
+    /**
+     * records down the information of the task passed in as parameters and put it into arrayList
+     * @param thisTask
+     * @param pane
+     */
+     private static void parseTaskObject(JSONObject thisTask, Pane pane)
+     {
+         //JSONObject thisTask = (JSONObject) task.get("Task");
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-    }
+         long tdate =  (long) thisTask.get("Date");
 
-    private static void parseTaskObject(JSONObject thisTask, Pane pane)
-    {
-        //JSONObject thisTask = (JSONObject) task.get("Task");
+         boolean tisDone = (boolean) thisTask.get("IsDone");
 
-        long tdate = (long) thisTask.get("Date");
+         String tname = (String) thisTask.get("Name");
 
-        boolean tisDone = (boolean) thisTask.get("IsDone");
+         long tmonth = (long) thisTask.get("Month");
 
-        String tname = (String) thisTask.get("Name");
+         long tdifficulty = (long) thisTask.get("Difficulty");
 
-        long tmonth = (long) thisTask.get("Month");
+         long ttaskID = (long) thisTask.get("taskID");
 
-        long tdifficulty = (long) thisTask.get("Difficulty");
+         Tasks tempTask = new Tasks( tname, tdate, tmonth, tisDone, tdifficulty, ttaskID, pane);
 
-        long ttaskID = (long) thisTask.get("taskID");
+         taskList.add(tempTask);
+     }
 
-        Tasks tempTask = new Tasks( tname, tdate, tmonth, tisDone, tdifficulty, ttaskID);
-
-        taskList.add(tempTask);
-
-        tempTask.showTask(pane);
-    }
 }
+
+
+
+
